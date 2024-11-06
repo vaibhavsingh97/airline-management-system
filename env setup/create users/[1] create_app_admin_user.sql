e2f89cbf479818b4a9c2fb616c09f34ab6b25997
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2024-11-06 15:57:11 -0500

    feat: create users from app_admin

---
+++ env setup/create users/[1] create_app_admin_user.sql
@@ -0,0 +1,62 @@
+/* 
+================================================
+‼️ THIS FILE SHOULD BE RUN BY DBA ADMIN ONLY ‼️
+================================================
+*/
+
+-- Create App Admin user
+SET SERVEROUTPUT ON
+/
+declare
+   user_exists number;
+   user_creation_sql varchar2(100);
+   user_drop_sql varchar2(100);
+   grant_access_sql varchar2(100);
+begin
+   -- SQL for dropping the user
+   user_drop_sql := 'DROP USER app_admin CASCADE';
+
+   -- SQL for creating the user
+   user_creation_sql := 'CREATE USER app_admin IDENTIFIED BY NeuBoston2024#';
+
+   grant_access_sql := 'GRANT CONNECT, SELECT ANY TABLE, DROP USER, CREATE USER TO app_admin';
+
+   -- Check is users exist
+   select count(*)
+     into user_exists
+     from all_users
+    where lower(username) = 'app_admin';
+
+   -- Drop the user if it exists
+   if user_exists > 0 then
+      begin
+         execute immediate user_drop_sql;
+         dbms_output.put_line('USER app_admin DROPPED SUCCESSFULLY ✅');
+      exception
+         when others then
+            dbms_output.put_line('❌ FAILED TO DROP USER app_admin:');
+            dbms_output.put_line(substr(sqlerrm, 11) || ' (Error Code: ' || sqlcode || ')');
+      end;
+   end if;
+
+      -- Attempt to create the user
+   begin
+      execute immediate user_creation_sql;
+      dbms_output.put_line('USER app_admin CREATED SUCCESSFULLY ✅');
+   exception
+      when others then
+         dbms_output.put_line('❌ FAILED TO CREATE USER app_admin:');
+         dbms_output.put_line(substr(sqlerrm, 11) || ' (Error Code: ' || sqlcode || ')');
+   end;
+
+   -- Grant access to the user
+   begin
+      execute immediate grant_access_sql;
+      dbms_output.put_line('Privileges granted to APP_ADMIN successfully. ✅');
+   exception
+      when others then
+         dbms_output.put_line('❌ Failed to grant privileges to app_admin:');
+         dbms_output.put_line(substr(sqlerrm, 11) || ' (Error Code: ' || sqlcode || ')');
+   end;
+end;
+/
---
+++ env setup/create users/[2] create_all_users.sql
@@ -0,0 +1,62 @@
+/* 
+================================================
+‼️ THIS FILE SHOULD BE RUN BY APP_ADMIN ONLY ‼️
+================================================
+*/
+
+declare
+   user_exists number;
+   
+   procedure create_user(username_data varchar2, password varchar2) is
+   begin
+      -- Check if the user already exists
+      select count(*)
+        into user_exists
+        from all_users
+       where lower(username) = lower(username_data);
+
+      -- Drop the user if it exists
+      if user_exists > 0 then
+         begin
+            execute immediate 'DROP USER ' || username_data || ' CASCADE';
+            dbms_output.put_line('USER ' || username_data || ' DROPPED SUCCESSFULLY ✅');
+         exception
+            when others then
+               dbms_output.put_line('❌ FAILED TO DROP USER ' || username_data || ':');
+               dbms_output.put_line(substr(sqlerrm, 11) || ' (Error Code: ' || sqlcode || ')');
+               raise; -- Stop execution if drop fails
+         end;
+      end if;
+
+      -- Attempt to create the user
+      begin
+         execute immediate 'CREATE USER ' || username_data || ' IDENTIFIED BY ' || password;
+         dbms_output.put_line('USER ' || username_data || ' CREATED SUCCESSFULLY ✅');
+         dbms_output.put_line(' ');
+      exception
+         when others then
+            dbms_output.put_line('❌ FAILED TO CREATE USER ' || username_data || ':');
+            dbms_output.put_line(substr(sqlerrm, 11) || ' (Error Code: ' || sqlcode || ')');
+            raise; -- Stop if user creation fails
+      end;
+   end;
+   
+begin
+   -- Create users with specified roles
+   create_user('DEVELOPER', 'NeuBoston2024#');
+   create_user('PASSENGER', 'NeuBoston2024#');
+   create_user('FLIGHT_OPERATION_MANAGER', 'NeuBoston2024#');
+   create_user('INVENTORY_MANAGER', 'NeuBoston2024#');
+   create_user('GROUND_OPERATION_MANAGER', 'NeuBoston2024#');
+   create_user('CREW_MANAGER', 'NeuBoston2024#');
+   create_user('MAINTENANCE_MANAGER', 'NeuBoston2024#');
+   create_user('HR_MANAGER', 'NeuBoston2024#');
+   create_user('DATA_ANALYST', 'NeuBoston2024#');
+
+   dbms_output.put_line('All users created or updated successfully.');
+   
+exception
+   when others then
+      dbms_output.put_line('An error occurred during user creation:');
+      dbms_output.put_line(substr(sqlerrm, 11) || ' (Error Code: ' || sqlcode || ')');
+end;
