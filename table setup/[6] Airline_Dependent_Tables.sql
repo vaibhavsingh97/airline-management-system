5dafb9236012f9f7728d9c0eeafaf6635f5c76a0
keyagoswami17 <goswami.ke@northeastern.edu>
2024-11-27 13:28:31 -0500

    Update create tables & add Trigger

--- table setup/[6] Airline_Dependent_Tables.sql
+++ table setup/[6] Airline_Dependent_Tables.sql
@@ -29,7 +29,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE inventory_order (
-                order_id               INTEGER NOT NULL,
+                order_id               INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 order_date             DATE NOT NULL,
                 order_quantity         INTEGER NOT NULL,
                 order_amount           NUMBER(10, 2) NOT NULL,
@@ -40,10 +40,6 @@ begin
                 updated_at             DATE NOT NULL
             )';
       dbms_output.put_line('Table inventory_order CREATED SUCCESSFULLY ✅');
-        
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE inventory_order ADD CONSTRAINT io_pk PRIMARY KEY ( order_id )';
-      dbms_output.put_line('Primary key constraint aircraft_pk added successfully ✅');
 
         -- Add foreign key constraint
       execute immediate 'ALTER TABLE inventory_order ADD CONSTRAINT io_inventory_fk FOREIGN KEY ( inventory_inventory_id )
@@ -51,7 +47,7 @@ begin
       dbms_output.put_line('Foreign key constraint io_inventory_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE inventory_order or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE inventory_order or foreign key ❌');
    end;
 end;
 
@@ -83,7 +79,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE passenger (
-                passenger_id     INTEGER NOT NULL,
+                passenger_id     INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 pass_first_name  VARCHAR2(20) NOT NULL,
                 pass_last_name   VARCHAR2(20) NOT NULL,
                 pass_email       VARCHAR2(50) NOT NULL,
@@ -96,10 +92,6 @@ begin
                 updated_at       DATE NOT NULL
             )';
       dbms_output.put_line('Table passenger CREATED SUCCESSFULLY ✅ ');
-        
-     -- Add primary key constraint
-      execute immediate 'ALTER TABLE passenger ADD CONSTRAINT passenger_pk PRIMARY KEY ( passenger_id )';
-      dbms_output.put_line('Primary key constraint passenger_pk added successfully ✅');
 
     -- Add passenger email constraint
       execute immediate 'ALTER TABLE passenger ADD CONSTRAINT chk_pass_email_format 
@@ -117,7 +109,7 @@ begin
       dbms_output.put_line('Foreign key wallet_id added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE aircraft or add primary key or phone or email or foreign key constraint ❌'
+         dbms_output.put_line('FAILED TO CREATE TABLE aircraft or phone or email or foreign key constraint ❌'
          );
    end;
 end;
@@ -150,7 +142,7 @@ begin
    begin
       execute immediate '
              CREATE TABLE refund (
-                refund_id          INTEGER NOT NULL,
+                refund_id          INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 refund_amount      NUMBER(10, 2) NOT NULL,
                 refund_reason      VARCHAR2(50) NOT NULL,
                 payment_payment_id INTEGER NOT NULL,
@@ -159,17 +151,13 @@ begin
             )';
       dbms_output.put_line('Table refund CREATED SUCCESSFULLY ✅');
         
-    -- Add primary key constraint
-      execute immediate 'ALTER TABLE refund ADD CONSTRAINT refund_pk PRIMARY KEY ( refund_id )';
-      dbms_output.put_line('Primary key constraint refund_pk added successfully ✅');
-        
     -- Add foreign key constraint
       execute immediate 'ALTER TABLE refund ADD CONSTRAINT refund_payment_fk FOREIGN KEY ( payment_payment_id )
         REFERENCES payment ( payment_id )';
       dbms_output.put_line('Foreign key constraint refund_payment_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE aircraft or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE aircraft or foreign key ❌');
    end;
 
 end;
@@ -204,7 +192,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE flight_schedule (
-                flight_schedule_id   INTEGER NOT NULL,
+                flight_schedule_id   INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 departure_airport    VARCHAR2(30) NOT NULL,
                 scheduled_dep_time   DATE NOT NULL,
                 actual_dep_time      DATE NOT NULL,
@@ -219,11 +207,7 @@ begin
             )';
       dbms_output.put_line('Table flight_schedule CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE flight_schedule ADD CONSTRAINT fs_pk PRIMARY KEY ( flight_schedule_id )';
-      dbms_output.put_line('Primary key constraint fs_pk added successfully ✅');
-
-        -- Add foreign key constraint
+      -- Add foreign key constraint
       execute immediate 'ALTER TABLE flight_schedule ADD CONSTRAINT fs_aircraft_fk FOREIGN KEY ( aircraft_aircraft_id )
         REFERENCES aircraft ( aircraft_id )';
       dbms_output.put_line('Foreign key constraint fs_aircraft_fk added successfully ✅');
@@ -232,7 +216,7 @@ begin
       dbms_output.put_line('Foreign key constraint fs_routes_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE flight_schedule or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE flight_schedule or foreign key ❌');
    end;
 
 end;
@@ -266,7 +250,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE maintenance_record (
-                main_record_id   INTEGER NOT NULL,
+                main_record_id   INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 main_record_date DATE NOT NULL,
                 main_record_type VARCHAR2(15) CHECK (main_record_type IN (''cleaning'',''routine'',''repairs'',''inspection'')) NOT NULL,
                 main_record_description VARCHAR2 (50)  NOT NULL,
@@ -279,10 +263,6 @@ begin
             )';
       dbms_output.put_line('Table maintenance_record CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE maintenance_record ADD CONSTRAINT mr_pk PRIMARY KEY ( main_record_id )';
-      dbms_output.put_line('Primary key constraint mr_pk added successfully ✅');
-
         -- Add foreign key constraint
       execute immediate 'ALTER TABLE maintenance_record ADD CONSTRAINT mr_aircraft_fk FOREIGN KEY ( aircraft_aircraft_id )
         REFERENCES aircraft ( aircraft_id )';
@@ -295,10 +275,10 @@ begin
       dbms_output.put_line('Foreign key constraint mr_inventory_fk added successfully ✅');
       execute immediate 'ALTER TABLE maintenance_record ADD CONSTRAINT mr_ms_fk FOREIGN KEY ( ms_main_schedule_id )
         REFERENCES maintenance_schedule ( main_schedule_id )';
-      dbms_output.put_line('Foreign key constraint mr_pk added successfully ✅');
+      dbms_output.put_line('Foreign key constraint mr_ms_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE maintenance_record or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE maintenance_record or foreign key ❌');
    end;
 
 end;
@@ -330,7 +310,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE ground_ops_schedule (
-                ops_schedule_id        INTEGER NOT NULL,
+                ops_schedule_id        INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 employee_employee_id   INTEGER NOT NULL,
                 mr_main_record_id      INTEGER NOT NULL,
                 inventory_inventory_id INTEGER NOT NULL,
@@ -339,9 +319,6 @@ begin
             )';
       dbms_output.put_line('Table ground_ops_schedule CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE ground_ops_schedule ADD CONSTRAINT gos_pk PRIMARY KEY ( ops_schedule_id )';
-      dbms_output.put_line('Primary key constraint gos_pk added successfully ✅');
         
         -- Add foreign key constraint
       execute immediate 'ALTER TABLE ground_ops_schedule ADD CONSTRAINT gos_employee_fk FOREIGN KEY ( employee_employee_id )
@@ -355,7 +332,7 @@ begin
       dbms_output.put_line('Foreign key constraint gos_mr_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE ground_ops_schedule or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE ground_ops_schedule or foreign key ❌');
    end;
 end;
 
@@ -387,7 +364,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE seat (
-                    seat_id               INTEGER NOT NULL,
+                    seat_id               INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                     seat_type             VARCHAR2(10) NOT NULL,
                     seat_class            VARCHAR2(20) NOT NULL,
                     is_available          CHAR(1) NOT NULL,
@@ -397,15 +374,13 @@ begin
             )';
       dbms_output.put_line('Table seat CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE seat ADD CONSTRAINT seat_pk PRIMARY KEY ( seat_id )';
-      dbms_output.put_line('Primary key constraint seat_pk added successfully ✅');
+      -- Add foreign key constraint
       execute immediate 'ALTER TABLE seat ADD CONSTRAINT seat_fs_fk FOREIGN KEY ( fs_flight_schedule_id )
         REFERENCES flight_schedule ( flight_schedule_id )';
       dbms_output.put_line('Foreign key constraint seat_fs_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE seat or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE seat or foreign key ❌');
    end;
 
 end;
@@ -439,7 +414,7 @@ begin
    begin
       execute immediate '
             CREATE TABLE reservation (
-                reservation_id         INTEGER NOT NULL,
+                reservation_id         INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 travel_date            DATE NOT NULL,
                 reservation_status     VARCHAR2(15) CHECK (reservation_status IN (''pending'',''confirmed'',''partial'',''cancelled'')) NOT NULL,
                 created_at             DATE NOT NULL,
@@ -454,10 +429,7 @@ begin
             )';
       dbms_output.put_line('Table reservation CREATED SUCCESSFULLY ✅');
         
-    -- Add primary key constraint
-      execute immediate 'ALTER TABLE reservation ADD CONSTRAINT reservation_pk PRIMARY KEY ( reservation_id )';
-      dbms_output.put_line('Primary key constraint reservation_pk added successfully ✅');
-    
+   
     -- Add foreign key constraint
       execute immediate 'ALTER TABLE reservation ADD CONSTRAINT reservation_seat_fk FOREIGN KEY ( seat_seat_id )
         REFERENCES seat ( seat_id )';
@@ -467,7 +439,7 @@ begin
       dbms_output.put_line('Foreign key constraint reservation_passenger_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE reservation or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE reservation or foreign key ❌');
    end;
 end;
 
@@ -499,24 +471,22 @@ begin
    begin
       execute immediate '
             CREATE TABLE baggage (
-                baggage_id             INTEGER NOT NULL,
+                baggage_id             INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 baggage_weight         NUMBER(4, 2) NOT NULL,
-                baggage_status         VARCHAR2(20) NOT NULL,
+                baggage_status         VARCHAR2(20) CHECK (baggage_status IN (''checkin'',''loaded'',''lost'',''unloaded'',''damaged'',''delivered'',''awaiting_transfer'',''claimed'')) NOT NULL,
                 reservation_reservation_id INTEGER NOT NULL,
                 created_at             DATE NOT NULL,
                 updated_at             DATE NOT NULL
             )';
       dbms_output.put_line('Table baggage CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE baggage ADD CONSTRAINT baggage_pk PRIMARY KEY ( baggage_id )';
-      dbms_output.put_line('Primary key constraint baggage_pk added successfully ✅');
+      -- Add foreign key constraint
       execute immediate 'ALTER TABLE baggage ADD CONSTRAINT bag_reservation_fk FOREIGN KEY ( reservation_reservation_id )
         REFERENCES reservation ( reservation_id )';
       dbms_output.put_line('Foreign key constraint bag_reservation_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE baggage or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE baggage or foreign key ❌');
    end;
 
 end;
@@ -550,7 +520,7 @@ begin
    begin
       execute immediate '
                 CREATE TABLE crew_assignment (
-                    assignment_id         INTEGER NOT NULL,
+                    assignment_id         INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                     assignment_date       DATE NOT NULL,
                     assignment_role       VARCHAR2(15) NOT NULL,
                     fs_flight_schedule_id INTEGER NOT NULL,
@@ -560,10 +530,6 @@ begin
                 )';
       dbms_output.put_line('Table crew_assignment CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE crew_assignment ADD CONSTRAINT ca_pk PRIMARY KEY ( assignment_id )';
-      dbms_output.put_line('Primary key constraint ca_pk added successfully ✅');
-
          -- Add foreign key constraint
       execute immediate 'ALTER TABLE crew_assignment ADD CONSTRAINT ca_employee_fk FOREIGN KEY ( employee_employee_id )
         REFERENCES employee ( employee_id )';
@@ -607,18 +573,13 @@ begin
    begin
       execute immediate '
              CREATE TABLE reservation_payment (
-                payment_payment_id         INTEGER NOT NULL,
+                payment_payment_id         INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                 reservation_reservation_id INTEGER NOT NULL,
                 created_at            DATE NOT NULL,
                 updated_at            DATE NOT NULL
             )';
       dbms_output.put_line('Table reservation_payment CREATED SUCCESSFULLY ✅');
         
-        -- Add primary key constraint
-      execute immediate 'ALTER TABLE reservation_payment ADD CONSTRAINT reservation_payment_pk PRIMARY KEY 
-        ( payment_payment_id,reservation_reservation_id )';
-      dbms_output.put_line('Primary key constraint reservation_payment_pk added successfully ✅'); 
-
         -- Add foreign key constraint
       execute immediate 'ALTER TABLE reservation_payment ADD CONSTRAINT res_pay_payment_fk FOREIGN KEY ( payment_payment_id )
         REFERENCES payment ( payment_id )';
@@ -628,7 +589,7 @@ begin
       dbms_output.put_line('Foreign key constraint res_pay_reservation_fk added successfully ✅');
    exception
       when others then
-         dbms_output.put_line('FAILED TO CREATE TABLE reservation_payment or add primary key or foreign key ❌');
+         dbms_output.put_line('FAILED TO CREATE TABLE reservation_payment or foreign key ❌');
    end;
 end;
 /
---
+++ trigger setup/[ ] flight_status_trigger.sql
@@ -0,0 +1,34 @@
+/*================================================
+‼️ THIS FILE SHOULD BE RUN BY DEVELOPER ONLY ‼️
+================================================*/
+
+CREATE OR REPLACE TRIGGER check_and_update_flight_status
+BEFORE INSERT OR UPDATE ON flight_schedule
+FOR EACH ROW
+BEGIN
+    -- Check if the actual departure time is delayed by more than 15 minutes
+    IF :NEW.actual_dep_time > :NEW.scheduled_dep_time + INTERVAL '15' MINUTE THEN
+        :NEW.flight_status := 'delayed'; -- Set the status to 'DELAYED'
+    END IF;
+END;
+/
+
+CREATE OR REPLACE TRIGGER check_route_within_one_hour
+BEFORE INSERT OR UPDATE ON flight_schedule
+FOR EACH ROW
+DECLARE
+    v_conflict_count NUMBER;
+BEGIN
+    -- Check if a flight is already scheduled for the same route within 1 hour
+    SELECT COUNT(*)
+    INTO v_conflict_count
+    FROM flight_schedule fs
+    WHERE fs.routes_route_id = :NEW.routes_route_id
+      AND ABS(TO_NUMBER(:NEW.scheduled_dep_time - fs.scheduled_dep_time) * 1440) <= 60;
+
+    -- If conflict exists, raise an error
+    IF v_conflict_count > 0 THEN
+        DBMS_OUTPUT.PUT_LINE('A flight is already scheduled for this route within 1 hour.');
+    END IF;
+END;
+/
