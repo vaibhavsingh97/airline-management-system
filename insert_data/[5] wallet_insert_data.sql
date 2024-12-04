50912deb0fbd40889a4a72a3f88025e655e5a93e
atharvah009 <hankare.a@northeastern.edu>
2024-12-03 21:20:16 -0500

    update insert procedures for wallet, passenger and seat

--- insert_data/[5] wallet_insert_data.sql
+++ insert_data/[5] wallet_insert_data.sql
@@ -4,11 +4,13 @@
 SET SERVEROUTPUT ON;
 
 CREATE OR REPLACE PROCEDURE insert_wallet (
+   p_membership_id      IN VARCHAR2,
    p_program_tier       IN VARCHAR2,
    p_points_earned      IN INTEGER,
    p_points_redeemed    IN INTEGER,
    p_transaction_id     IN VARCHAR2,
    p_transaction_reason IN VARCHAR2,
+   p_passenger_passenger_id IN VARCHAR2,
    p_created_at         IN VARCHAR2,
    p_updated_at         IN VARCHAR2
 ) IS
@@ -24,19 +26,23 @@ BEGIN
    -- If a record doesn't exist, insert a new record
    IF v_count = 0 THEN
       INSERT INTO wallet (
+         membership_id,
          program_tier,
          points_earned,
          points_redeemed,
          transaction_id,
          transaction_reason,
+         passenger_passenger_id,
          created_at,
          updated_at
       ) VALUES ( 
+         p_membership_id,
          p_program_tier,
          p_points_earned,
          p_points_redeemed,
          p_transaction_id,
          p_transaction_reason,
+         p_passenger_passenger_id,
          TO_DATE(p_created_at, 'YYYY-MM-DD HH24:MI:SS'),
          TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS') 
       ) RETURNING wallet_id INTO v_wallet_id;
@@ -58,47 +64,79 @@ END insert_wallet;
 begin
     -- Inserting data into wallet table
    insert_wallet(
+      'HN321',
       'platinum',
-      18698,
-      1202,
+      100,
+      0,
       '30466',
       'credit',
+      1,
       '2023-01-01 09:05:30',
       '2023-01-01 09:05:30'
    );
    insert_wallet(
+      'GD787',
       'platinum',
-      15594,
-      6195,
+      400,
+      0,
       '83242',
       'credit',
+      2,
       '2023-02-18 15:25:44',
       '2023-02-18 15:25:44'
    );
    insert_wallet(
+      'PF498',
       'gold',
-      15612,
-      17814,
+      200,
+      0,
       '22330',
-      'debit',
+      'credit',
+      3,
       '2023-04-05 10:16:12',
       '2023-04-05 10:16:12'
    );
    insert_wallet(
+      'OC396',
       'silver',
-      11443,
-      14835,
+      600,
+      0,
       '20140',
-      'debit',
+      'credit',
+      4,
       '2023-06-19 07:50:03',
       '2023-06-19 07:50:03'
    );
    insert_wallet(
+      'DP912',
       'platinum',
-      1840,
-      19177,
+      700,
+      0,
       '87732',
+      'credit',
+      5,
+      '2023-08-30 12:11:09',
+      '2023-08-30 12:11:09'
+   );
+   insert_wallet(
+      'PF498',
+      'gold',
+      0,
+      100,
+      '22331',
       'debit',
+      3,
+      '2023-04-05 10:16:12',
+      '2023-04-05 10:16:12'
+   );
+   insert_wallet(
+      'DP912',
+      'platinum',
+      0,
+      400,
+      '87733',
+      'debit',
+      5,
       '2023-08-30 12:11:09',
       '2023-08-30 12:11:09'
    );
--- insert_data/[8] passenger_insert_data.sql
+++ insert_data/[8] passenger_insert_data.sql
@@ -4,16 +4,15 @@
 SET SERVEROUTPUT ON;
 
 CREATE OR REPLACE PROCEDURE insert_passenger (
-   p_pass_first_name IN VARCHAR2,
-   p_pass_last_name  IN VARCHAR2,
-   p_pass_email      IN VARCHAR2,
-   p_pass_phone      IN NUMBER,
-   p_gender          IN VARCHAR2,
-   p_dob             IN VARCHAR2,
-   p_seat_preference IN VARCHAR2,
-   p_created_at      IN VARCHAR2,
-   p_updated_at      IN VARCHAR2,
-   p_wallet_wallet_id IN VARCHAR2
+   p_pass_first_name          IN VARCHAR2,
+   p_pass_last_name           IN VARCHAR2,
+   p_pass_email               IN VARCHAR2,
+   p_pass_phone               IN NUMBER,
+   p_gender                   IN VARCHAR2,
+   p_dob                      IN VARCHAR2,
+   p_seat_preference          IN VARCHAR2,
+   p_created_at               IN VARCHAR2,
+   p_updated_at               IN VARCHAR2
 ) IS
    v_count NUMBER;
    v_passenger_id INTEGER;
@@ -33,8 +32,7 @@ BEGIN
           gender = p_gender,
           dob = p_dob,
           seat_preference = p_seat_preference,
-          updated_at = TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS'),
-          wallet_wallet_id = p_wallet_wallet_id
+          updated_at = TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS')
       WHERE pass_email = p_pass_email
       RETURNING passenger_id INTO v_passenger_id;
       
@@ -50,8 +48,7 @@ BEGIN
          dob,
          seat_preference,
          created_at,
-         updated_at,
-         wallet_wallet_id
+         updated_at
       ) VALUES (
          p_pass_first_name,
          p_pass_last_name,
@@ -61,8 +58,7 @@ BEGIN
          p_dob,
          p_seat_preference,
          TO_DATE(p_created_at, 'YYYY-MM-DD HH24:MI:SS'),
-         TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS'),
-         p_wallet_wallet_id
+         TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS')
       ) RETURNING passenger_id INTO v_passenger_id;
       
       DBMS_OUTPUT.PUT_LINE('✅ Successfully inserted passenger with ID: ' || v_passenger_id);
@@ -87,8 +83,7 @@ begin
       '10-05-1931',
       'aisle',
       '2022-09-23 00:53:23',
-      '2022-09-23 00:53:23',
-      null
+      '2022-09-23 00:53:23'
    );
    insert_passenger(
       'Tabby',
@@ -99,8 +94,7 @@ begin
       '15-05-1957',
       'middle',
       '2023-07-16 03:07:23',
-      '2023-07-16 03:07:23',
-      null
+      '2023-07-16 03:07:23'
    );
    insert_passenger(
       'Kitty',
@@ -111,11 +105,9 @@ begin
       '10-01-1989',
       'aisle',
       '2023-03-25 04:11:48',
-      '2023-03-25 04:11:48',
-      null
+      '2023-03-25 04:11:48'
    );
    insert_passenger(
-      804,
       'Eda',
       'Borham',
       'eborham3@jiathis.com',
@@ -124,8 +116,7 @@ begin
       '31-07-1964',
       'middle',
       '2024-07-02 13:08:56',
-      '2024-07-02 13:08:56',
-      null
+      '2024-07-02 13:08:56'
    );
    insert_passenger(
       'Pennie',
@@ -136,68 +127,7 @@ begin
       '08-08-1982',
       'window',
       '2024-06-11 05:39:51',
-      '2024-06-11 05:39:51',
-      null
-   );
-   insert_passenger(
-      'Daven',
-      'Krollman',
-      'dkrollman5@chronoengine.com',
-      '2117070929',
-      'male',
-      '25-08-1996',
-      'window',
-      '2024-07-03 00:27:07',
-      '2024-07-03 00:27:07',
-      null
-   );
-   insert_passenger(
-      'Sigismondo',
-      'Marnane',
-      'smarnane6@multiply.com',
-      '4906837346',
-      'male',
-      '13-09-2003',
-      'window',
-      '2023-08-06 00:35:55',
-      '2023-08-06 00:35:55',
-      null
-   );
-   insert_passenger(
-      'Jessalyn',
-      'Underdown',
-      'junderdown7@liveinternet.ru',
-      '1091457252',
-      'female',
-      '03-01-1984',
-      'middle',
-      '2022-12-21 08:43:43',
-      '2022-12-21 08:43:43',
-      null
-   );
-   insert_passenger(
-      'Brucie',
-      'Heersema',
-      'bheersema8@nhs.uk',
-      '2506837379',
-      'male',
-      '03-12-1943',
-      'aisle',
-      '2024-04-21 22:05:36',
-      '2024-04-21 22:05:36',
-      null
-   );
-   insert_passenger(
-      'Ebeneser',
-      'Gravet',
-      'egravet9@furl.net',
-      '5616473517',
-      'male',
-      '15-01-1947',
-      'aisle',
-      '2024-04-10 20:30:02',
-      '2024-04-10 20:30:02',
-      null
+      '2024-06-11 05:39:51'
    );
    commit;
 end;
