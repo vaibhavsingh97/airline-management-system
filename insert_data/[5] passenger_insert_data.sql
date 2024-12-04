ec1590564f6c94ec94713c78cac01685c8ef22d7
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2024-12-04 11:40:52 -0500

    Revert "chore: revert passenger_insert_data.sql"
    This reverts commit 60d642825860f44cf36ef44088635d524dc11b95.

--- insert_data/[5] passenger_insert_data.sql
+++ insert_data/[5] passenger_insert_data.sql
@@ -4,16 +4,15 @@
 SET SERVEROUTPUT ON;
 
 CREATE OR REPLACE PROCEDURE insert_passenger (
-   p_pass_first_name IN VARCHAR2,
-   p_pass_last_name  IN VARCHAR2,
-   p_pass_email      IN VARCHAR2,
-   p_pass_phone      IN NUMBER,
-   p_gender          IN VARCHAR2,
-   p_dob             IN VARCHAR2,
-   p_seat_preference IN VARCHAR2,
-   p_created_at      IN VARCHAR2,
-   p_updated_at      IN VARCHAR2,
-   p_wallet_wallet_id IN VARCHAR2
+   p_pass_first_name          IN VARCHAR2,
+   p_pass_last_name           IN VARCHAR2,
+   p_pass_email               IN VARCHAR2,
+   p_pass_phone               IN NUMBER,
+   p_gender                   IN VARCHAR2,
+   p_dob                      IN VARCHAR2,
+   p_seat_preference          IN VARCHAR2,
+   p_created_at               IN VARCHAR2,
+   p_updated_at               IN VARCHAR2
 ) IS
    v_count NUMBER;
    v_passenger_id INTEGER;
@@ -33,8 +32,7 @@ BEGIN
           gender = p_gender,
           dob = p_dob,
           seat_preference = p_seat_preference,
-          updated_at = TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS'),
-          wallet_wallet_id = p_wallet_wallet_id
+          updated_at = TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS')
       WHERE pass_email = p_pass_email
       RETURNING passenger_id INTO v_passenger_id;
       
@@ -50,19 +48,17 @@ BEGIN
          dob,
          seat_preference,
          created_at,
-         updated_at,
-         wallet_wallet_id
+         updated_at
       ) VALUES (
          p_pass_first_name,
          p_pass_last_name,
          p_pass_email,
          p_pass_phone,
          p_gender,
-         p_dob,
+         TO_DATE(p_dob,'DD-MM-YYYY'),
          p_seat_preference,
          TO_DATE(p_created_at, 'YYYY-MM-DD HH24:MI:SS'),
-         TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS'),
-         p_wallet_wallet_id
+         TO_DATE(p_updated_at, 'YYYY-MM-DD HH24:MI:SS')
       ) RETURNING passenger_id INTO v_passenger_id;
       
       DBMS_OUTPUT.PUT_LINE('✅ Successfully inserted passenger with ID: ' || v_passenger_id);
@@ -71,9 +67,7 @@ BEGIN
    COMMIT; -- Commit the transaction
 EXCEPTION
    WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error Code: ' || SQLCODE);
-      DBMS_OUTPUT.PUT_LINE('Error Message: ' || SQLERRM);
-      RAISE_APPLICATION_ERROR(-20001, '❌ Failed to insert or update passenger: ' || p_pass_email);
+      DBMS_OUTPUT.PUT_LINE('❌ Failed to insert or update passenger: ' || p_pass_email);
 END insert_passenger;
 /
 
@@ -81,7 +75,6 @@ END insert_passenger;
 begin
     -- Inserting data into passenger table
    insert_passenger(
-      801,
       'Catina',
       'Mellers',
       'cmellers0@tuttocitta.it',
@@ -90,11 +83,9 @@ begin
       '10-05-1931',
       'aisle',
       '2022-09-23 00:53:23',
-      '2022-09-23 00:53:23',
-      null
+      '2022-09-23 00:53:23'
    );
    insert_passenger(
-      802,
       'Tabby',
       'Pegg',
       'tpegg1@google.de',
@@ -103,11 +94,9 @@ begin
       '15-05-1957',
       'middle',
       '2023-07-16 03:07:23',
-      '2023-07-16 03:07:23',
-      502
+      '2023-07-16 03:07:23'
    );
    insert_passenger(
-      803,
       'Kitty',
       'Warlawe',
       'kwarlawe2@ibm.com',
@@ -116,11 +105,9 @@ begin
       '10-01-1989',
       'aisle',
       '2023-03-25 04:11:48',
-      '2023-03-25 04:11:48',
-      503
+      '2023-03-25 04:11:48'
    );
    insert_passenger(
-      804,
       'Eda',
       'Borham',
       'eborham3@jiathis.com',
@@ -129,11 +116,9 @@ begin
       '31-07-1964',
       'middle',
       '2024-07-02 13:08:56',
-      '2024-07-02 13:08:56',
-      504
+      '2024-07-02 13:08:56'
    );
    insert_passenger(
-      805,
       'Pennie',
       'Jouandet',
       'pjouandet4@sphinn.com',
@@ -142,73 +127,7 @@ begin
       '08-08-1982',
       'window',
       '2024-06-11 05:39:51',
-      '2024-06-11 05:39:51',
-      505
-   );
-   insert_passenger(
-      806,
-      'Daven',
-      'Krollman',
-      'dkrollman5@chronoengine.com',
-      '2117070929',
-      'male',
-      '25-08-1996',
-      'window',
-      '2024-07-03 00:27:07',
-      '2024-07-03 00:27:07',
-      null
-   );
-   insert_passenger(
-      807,
-      'Sigismondo',
-      'Marnane',
-      'smarnane6@multiply.com',
-      '4906837346',
-      'male',
-      '13-09-2003',
-      'window',
-      '2023-08-06 00:35:55',
-      '2023-08-06 00:35:55',
-      507
-   );
-   insert_passenger(
-      808,
-      'Jessalyn',
-      'Underdown',
-      'junderdown7@liveinternet.ru',
-      '1091457252',
-      'female',
-      '03-01-1984',
-      'middle',
-      '2022-12-21 08:43:43',
-      '2022-12-21 08:43:43',
-      508
-   );
-   insert_passenger(
-      809,
-      'Brucie',
-      'Heersema',
-      'bheersema8@nhs.uk',
-      '2506837379',
-      'male',
-      '03-12-1943',
-      'aisle',
-      '2024-04-21 22:05:36',
-      '2024-04-21 22:05:36',
-      509
-   );
-   insert_passenger(
-      810,
-      'Ebeneser',
-      'Gravet',
-      'egravet9@furl.net',
-      '5616473517',
-      'male',
-      '15-01-1947',
-      'aisle',
-      '2024-04-10 20:30:02',
-      '2024-04-10 20:30:02',
-      null
+      '2024-06-11 05:39:51'
    );
    commit;
 end;
