4fe14d78367f301717a67a58618e6a46a2690063
keyagoswami17 <goswami.ke@northeastern.edu>
2024-12-04 21:02:01 -0500

    chore: update crew assignment trigger

--- package/[ ] staff_management.sql
+++ package/[ ] staff_management.sql
@@ -4,6 +4,7 @@
 ================================================
 */
 
+alter session set nls_date_format='YYYY-MM-DD HH24:MI:SS';
 
 CREATE OR REPLACE PACKAGE staff_management AS
     -- Procedure to register a staff member
